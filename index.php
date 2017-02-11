53de77297dbaf04e565bb62107205093a982636f
Piret Spitsõn <sppiret@gmail.com>
2017-02-12 00:10:07 +0200

    Lisatud kell

--- index.php
+++ index.php
@@ -4,9 +4,27 @@
 	<meta charset="UTF-8">
 		<title>Pireti kodukas</title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
+	<script>
+		//Võetud http://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
+		function startTime() {
+			var today = new Date();
+			var h = today.getHours();
+			var m = today.getMinutes();
+			var s = today.getSeconds();
+			m = checkTime(m);
+			s = checkTime(s);
+			document.getElementById('txt').innerHTML =
+			h + ":" + m + ":" + s;
+			var t = setTimeout(startTime, 500);
+		}
+		function checkTime(i) {
+			if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+			return i;
+		}
+		</script>
 	</head>
 	
-	<body>
+	<body onload="startTime()">
 	<?php echo phpversion()?>
 		<h1>Tere, maailm ning muud karvased ja sulelised!</h1>
 		
@@ -19,10 +37,11 @@
 		<!--See jätab tühja rea-->
 		<p>&nbsp;</p>	
 		
-		<p><font size=6>Testing-testing 1-2-3. Siin Piret, kes testib kodukat.</font></p>
+		<p><font size=6>Pireti kodulehekülg aines võrgurakendused I.</font></p>
 		<p></p>
 		<p></p>
 		
+		<div id="txt">
+		</div>
 	</body>
-	
 </html>
